22fa7e55340a410cbeafb380105a43d9a2dd1ac6
kwonseokki <kwons41635841@gmail.com>
2026-09-23 17:51:28 +0900

    게시물 상세조회 컨트롤러 연결

--- src/main/java/std/reply/controller/BoardController.java
+++ src/main/java/std/reply/controller/BoardController.java
@@ -1,17 +1,31 @@
 package std.reply.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import std.reply.dto.BoardDTO;
+import std.reply.service.BoardService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board")
+@AllArgsConstructor
 @Slf4j
 public class BoardController {
 
+    private final BoardService boardService;
+
     @RequestMapping("/list")
-    public void getBoardList() {
-      log.info("board/list");
+    public List<BoardDTO> getBoardList() {
+      return boardService.getBoardList();
+    }
+
+    @RequestMapping("/{id}")
+    public BoardDTO getBoardDetail(@PathVariable("id") int id) {
+        return boardService.getBoard(id);
     }
 }
